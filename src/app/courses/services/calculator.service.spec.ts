06ef9df78413a2d95951e2f903ccc9d5ab4b7844
Andre Moura <andremoura500@gmail.com>
2022-08-26 21:32:03 -0300

    test: add spyOn in CalclatorService

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,8 +3,15 @@ import { LoggerService } from "./logger.service";
 
 describe('Calculatorservice', () => {
 
+    // com spyOn
+
     it('should add two numbers', () => {
-        const calculator = new CalculatorService(new LoggerService());
+        const logger = jasmine.createSpyObj('LoggerService', ["log"]);
+
+        // quando precisar retornar valores de um metodo fake, do it:
+        // logger.log.and.returnValue();
+
+        const calculator = new CalculatorService(logger);
         const result = calculator.add(2, 2);
         expect(result).toBe(4);
 
@@ -16,5 +23,21 @@ describe('Calculatorservice', () => {
         expect(result).toBe(0, "resultado inesperado de subtração");
 
     });
+
+    // Básico
+
+    // it('should add two numbers', () => {
+    //     const calculator = new CalculatorService(new LoggerService());
+    //     const result = calculator.add(2, 2);
+    //     expect(result).toBe(4);
+
+    // });
+
+    // it('should subtract two numbers', () => {
+    //     const calculator = new CalculatorService(new LoggerService());
+    //     const result = calculator.subtract(2, 2);
+    //     expect(result).toBe(0, "resultado inesperado de subtração");
+
+    // });
 
 })
