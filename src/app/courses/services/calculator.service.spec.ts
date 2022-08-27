b2ebf07e07f5760292f0cf7bf796d351634350a8
Andre Moura <andremoura500@gmail.com>
2022-08-26 21:07:09 -0300

    feat: calculatorService spec, pending e fail

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,11 @@
+describe('Calculatorservice', () => {
+
+    it('should add two numbers', () => {
+        pending();
+    });
+
+    it('should subtract two numbers', () => {
+        fail();
+    });
+
+})
