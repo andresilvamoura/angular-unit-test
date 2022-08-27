deab77e830d0505a5103a0c1fb0d8558c6c784a6
Andre Moura <andremoura500@gmail.com>
2022-08-26 22:28:02 -0300

    test: add structure in service with httpclient

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,3 +1,4 @@
+import { TestBed } from "@angular/core/testing";
 import { any } from "cypress/types/bluebird";
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
@@ -6,10 +7,17 @@ describe('Calculatorservice', () => {
 
     let calculator: CalculatorService;
     let loggerSpy: any;
-    
+
     beforeEach(() => {
         loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
-        calculator = new CalculatorService(loggerSpy);
+        
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                { provide: LoggerService, useValue: loggerSpy }
+            ],
+        })
+        calculator = TestBed.inject(CalculatorService);
     })
 
     // com spyOn
---
+++ src/app/courses/services/courses.service.spec.ts
@@ -0,0 +1,58 @@
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+import { CoursesService } from "./courses.service";
+
+
+describe('CoursesService', () => {
+
+    let coursesService: CoursesService;
+    let httpTestingController: HttpTestingController;
+    beforeEach(() => {
+
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule
+            ],
+            providers: [
+                CoursesService,
+            ]
+        })
+
+        coursesService = TestBed.inject(CoursesService);
+        httpTestingController = TestBed.inject(HttpTestingController);
+    })    
+
+    it('should retrieve all courses', () => {
+        
+    });
+});
+
+
+
+// =====================================================================================
+        // ESTRUTURA PARA TESTAR SERVIÇOS COM HTTPCLIENT
+// =====================================================================================
+
+// describe('CoursesService', () => {
+
+//     let coursesService: CoursesService;
+//     let httpTestingController: HttpTestingController;
+//     beforeEach(() => {
+
+//         TestBed.configureTestingModule({
+//             imports: [
+//                 HttpClientTestingModule
+//             ],
+//             providers: [
+//                 CoursesService,
+//             ]
+//         })
+
+//         coursesService = TestBed.inject(CoursesService);
+//         httpTestingController = TestBed.inject(HttpTestingController);
+//     })    
+
+//     it('should retrieve all courses', () => {
+        
+//     });
+// });
